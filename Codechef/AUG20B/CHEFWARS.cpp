1465d1f6e67eb38492d6cca6b6ce5cab18ff6437
Prayush Jain <52440592+jainprayush@users.noreply.github.com>
2020-08-09 11:08:25 +0530

    Problem with link

--- Codechef/AUG20B/CHEFWARS.cpp
+++ Codechef/AUG20B/CHEFWARS.cpp
@@ -1,2 +1,23 @@
 //Link to the problem: https://www.codechef.com/AUG20B/problems/CHEFWARS
 
+#include<iostream>
+ using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long int T;
+    cin>>T;
+    while(T--)
+    {
+        long int H,P;
+        cin>>H>>P;
+        if((H/2)>P)
+            cout<<0<<"\n";
+        else
+            cout<<1<<"\n";
+    }
+    
+}
+
